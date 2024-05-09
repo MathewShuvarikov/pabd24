88391150ff34bb095ae59da45057fcaa2d014b3e
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-05-09 23:33:43 +0300

    add changes to parse_cian.py and to .gitignore

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -1,27 +1,44 @@
+import datetime
 import cianparser
 import pandas as pd
-import datetime
 from dotenv import dotenv_values
+import boto3
 
-# using now() to get current time
-configs = dotenv_values('.env')
+YOUR_ID = '28'
+
+config = dotenv_values(".env")
+client = boto3.client(
+    's3',
+    endpoint_url='https://storage.yandexcloud.net',
+    aws_access_key_id=config['KEY'],
+    aws_secret_access_key=config['SECRET']
+)
 
 moscow_parser = cianparser.CianParser(location="Москва")
 
+
 def main():
-    rooms = 3
+    t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    n_rooms = 3
+    CSV_PATH = f'data/raw/{n_rooms}_{t}.csv'
     data = moscow_parser.get_flats(
         deal_type="sale",
-        rooms=(rooms,),
+        rooms=(n_rooms,),
         with_saving_csv=False,
         additional_settings={
             "start_page": 1,
             "end_page": 50,
             "object_type": "secondary"
         })
-    data = pd.DataFrame(data)
-    t = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    data.to_csv(f'../data/raw/rooms_{rooms}_{t}.csv', index=0)
+    df = pd.DataFrame(data)
+
+    df.to_csv(CSV_PATH,
+              encoding='utf-8',
+              index=False)
+    bucket_name = 'pabd24'
+    object_name = f'{YOUR_ID}/' + CSV_PATH
+    client.upload_file(CSV_PATH, bucket_name, object_name)
+
 
 if __name__ == '__main__':
     main()
