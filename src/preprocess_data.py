5ac82b30c04f40f60f318daffb45e87ef3b4f1c6
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-05-27 19:03:03 +0300

    update files

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -39,8 +39,8 @@ def main(args):
     df.dropna(inplace=True)
     df.drop(columns=['price_per_month', 'commissions'], inplace=True)
     print('dataframe shape', df.shape)
-    # df = df.loc[df.price<=30_000_000,:]
-    df['elite_estate'] = np.where(df.price<=30_000_000, 0, 1)
+    df = df.loc[df.price<=30_000_000,:]
+    # df['elite_estate'] = np.where(df.price<=30_000_000, 0, 1)
 
     train_df, val_df = train_test_split(df, train_size=TRAIN_SIZE, random_state=1, shuffle=True)
     train_df.to_csv(OUT_TRAIN, index=0)
