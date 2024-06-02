684c668a66be70c2e3f26ac5b6ba51b986e6db78
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-06-02 23:28:10 +0300

    update files

--- src/predict_app.py
+++ src/predict_app.py
@@ -60,7 +60,7 @@ def predict(in_data: dict) -> int:
     prediction = model.predict(data)
 
     # return f"{int(np.exp(prediction[0])):,.0f}, +- 2.5 mln"
-    return predict_io_bounded(data['total_meters'])
+    return predict_cpu_bounded(data['total_meters'], n=5_000_000)
 
 @app.route("/")
 def home():
