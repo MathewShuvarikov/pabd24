90b9d083bdf58918a0cd415787f85e7bd7670d78
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-06-14 14:30:29 +0300

    update files

--- src/predict_app.py
+++ src/predict_app.py
@@ -39,41 +39,40 @@ def predict(in_data: dict) -> int:
     :return: House price, RUB.
     :rtype: int
     """
-    #
-    # data = pd.DataFrame(in_data, index=[0])
-    #
-    # if 'county_short' not in data.columns:
-    #
-    #     mapping = pd.read_csv('mapping/county.txt', sep='|')
-    #     data = data.merge(mapping, left_on='district', right_on='district_name', how='left')
-    #     data['county_short'] = np.where(data['county_short'].isna(), 'unknown', data['county_short'])
-    #     data['county_short'] = np.where(data['county_short'] == 'Марьина роща', 'СВАО', data['county_short'])
-    #
-    # data[['total_meters', 'rooms_count', 'floor', 'floors_count']] = data[['total_meters', 'rooms_count', 'floor', 'floors_count']].astype(float)
-    # data['top_bottom_floor'] = np.where((data.floor == data.floors_count) | (data.floor == 1), 1, 0)
-    # data = pd.get_dummies(data, columns=['county_short'], dtype=int)
-    #
-    # need_cols = ['floor', 'floors_count', 'rooms_count', 'total_meters', 'top_bottom_floor', 'county_short_ВАО', 'county_short_ЗАО',
-    #  'county_short_ЗелАО', 'county_short_САО', 'county_short_СВАО', 'county_short_СЗАО', 'county_short_ТАО',
-    #  'county_short_ЦАО', 'county_short_ЮАО', 'county_short_ЮВАО', 'county_short_ЮЗАО', 'object_type_secondary']
-    #
-    # missing = list(set(need_cols) - set(data.columns))
-    # for i in missing:
-    #     data[i] = 0
-    #
-    # data['object_type_secondary'] = np.where(data.object_type == 'secondary', 1, 0)
-    # data.drop(columns='object_type', inplace=True)
-    #
-    # data = data[need_cols]
-    # prediction = model.predict(data)
-    #
-    # upper = int(np.exp(prediction[0])) + 2_500_000
-    # lower = int(np.exp(prediction[0])) - 2_500_000
 
-    res = predict_io_bounded(in_data['total_meters'])
+    data = pd.DataFrame(in_data, index=[0])
 
-    # return int(np.exp(prediction[0]))
-    return res
+    if 'county_short' not in data.columns:
+
+        mapping = pd.read_csv('mapping/county.txt', sep='|')
+        data = data.merge(mapping, left_on='district', right_on='district_name', how='left')
+        data['county_short'] = np.where(data['county_short'].isna(), 'unknown', data['county_short'])
+        data['county_short'] = np.where(data['county_short'] == 'Марьина роща', 'СВАО', data['county_short'])
+
+    data[['total_meters', 'rooms_count', 'floor', 'floors_count']] = data[['total_meters', 'rooms_count', 'floor', 'floors_count']].astype(float)
+    data['top_bottom_floor'] = np.where((data.floor == data.floors_count) | (data.floor == 1), 1, 0)
+    data = pd.get_dummies(data, columns=['county_short'], dtype=int)
+
+    need_cols = ['floor', 'floors_count', 'rooms_count', 'total_meters', 'top_bottom_floor', 'county_short_ВАО', 'county_short_ЗАО',
+     'county_short_ЗелАО', 'county_short_САО', 'county_short_СВАО', 'county_short_СЗАО', 'county_short_ТАО',
+     'county_short_ЦАО', 'county_short_ЮАО', 'county_short_ЮВАО', 'county_short_ЮЗАО', 'object_type_secondary']
+
+    missing = list(set(need_cols) - set(data.columns))
+    for i in missing:
+        data[i] = 0
+
+    data['object_type_secondary'] = np.where(data.object_type == 'secondary', 1, 0)
+    data.drop(columns='object_type', inplace=True)
+
+    data = data[need_cols]
+    prediction = model.predict(data)
+
+    upper = int(np.exp(prediction[0])) + 2_500_000
+    lower = int(np.exp(prediction[0])) - 2_500_000
+
+    # res = predict_cpu_bounded(in_data['total_meters'], n=)
+
+    return int(np.exp(prediction[0]))
 
 @app.route("/")
 def home():
