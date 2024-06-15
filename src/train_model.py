5eab67b25beaec82d510d119e145e62a658e4b18
MathewShuvarikov <shuvarikoff20@gmail.com>
2024-06-15 08:35:20 +0300

    update files

--- src/train_model.py
+++ src/train_model.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 
 TRAIN_DATA = 'data/proc/train.csv'
 VAL_DATA = 'data/proc/val.csv'
-MODEL_SAVE_PATH = 'models/lgbm.joblib'
+MODEL_SAVE_PATH = 'models/rf.joblib'
 
 
 def main(args):
@@ -35,28 +35,28 @@ def main(args):
     # logger.info(f'Saved to {args.model}')
     # r2 = linear_model.score(x_train, y_train)
 
-    # rf = RandomForestRegressor(random_state=0, n_jobs=-1,
-    #                            ** {'max_depth': 9, 'max_features': 'sqrt', 'min_samples_leaf': 1, 'min_samples_split': 2})
-    # rf.fit(x_train, y_train)
-    # r2 = rf.score(x_train, y_train)
-    # dump(rf, args.model)
-    # logger.info(f'Saved to {args.model}')
-
-    lgbm = LGBMRegressor(random_state=0, n_jobs=-1,**{'n_estimators': 86, 'max_depth': 5, 'learning_rate': 0.24681488010947256,
-                                                      'num_leaves': 14, 'reg_alpha': 1.0374405840419012, 'reg_lambda': 28.361258497509773})
-# **{'n_estimators': 269, 'max_depth': 9, 'learning_rate': 0.05096330723976655,
-#   'num_leaves': 98, 'reg_alpha': 0.17995860538849384, 'reg_lambda': 30.29993490458629})
-    lgbm.fit(x_train, y_train)
-    r2 = lgbm.score(x_train, y_train)
-    mape1 = mape(np.exp(y_train), np.exp(lgbm.predict(x_train)))
-    mae1 = mae(np.exp(y_train), np.exp(lgbm.predict(x_train)))
-    dump(lgbm, args.model)
+    rf = RandomForestRegressor(random_state=0, n_jobs=-1,
+                               ** {'max_depth': 9, 'max_features': 'sqrt', 'min_samples_leaf': 1, 'min_samples_split': 2})
+    rf.fit(x_train, y_train)
+    r2 = rf.score(x_train, y_train)
+    dump(rf, args.model)
     logger.info(f'Saved to {args.model}')
+
+#     lgbm = LGBMRegressor(random_state=0, n_jobs=-1,**{'n_estimators': 86, 'max_depth': 5, 'learning_rate': 0.24681488010947256,
+#                                                       'num_leaves': 14, 'reg_alpha': 1.0374405840419012, 'reg_lambda': 28.361258497509773})
+# # **{'n_estimators': 269, 'max_depth': 9, 'learning_rate': 0.05096330723976655,
+# #   'num_leaves': 98, 'reg_alpha': 0.17995860538849384, 'reg_lambda': 30.29993490458629})
+#     lgbm.fit(x_train, y_train)
+#     r2 = lgbm.score(x_train, y_train)
+#     mape1 = mape(np.exp(y_train), np.exp(lgbm.predict(x_train)))
+#     mae1 = mae(np.exp(y_train), np.exp(lgbm.predict(x_train)))
+#     dump(lgbm, args.model)
+#     logger.info(f'Saved to {args.model}')
 
     # c = int(linear_model.coef_[0])
     # inter = int(linear_model.intercept_)
 
-    logger.info(f'R2 = {r2:.3f}  MAE = {mae1:,.0f} MAPE = {mape1:.3f}')
+    # logger.info(f'R2 = {r2:.3f}  MAE = {mae1:,.0f} MAPE = {mape1:.3f}')
 
 
 if __name__ == '__main__':
